9de6ae5093f6a7dcb011ebd742254009da1d24d5
nutawut2539 <34149513+nutawut2539@users.noreply.github.com>
2018-02-12 14:51:37 +0700

    Update index.php

--- index.php
+++ index.php
@@ -20,7 +20,7 @@
       if(empty($errors)==true){
          echo "Success\n";
 	$a = $_FILES['image']['name'];
-	//models
+	//models not work sometimes
 	$predict = shell_exec("python models-master/tutorials/image/imagenet/classify_image.py --image_file $a ");
 	//trained tensorflow
 	$train = shell_exec("python tensorflow-master/tensorflow/examples/label_image/label_image.py \--graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt \--input_layer=Mul \--output_layer=final_result \--input_mean=128 --input_std=128 \--image=$a");
