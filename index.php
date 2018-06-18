302798f8fa1c8a67a573a7e2584bf1c090adb5d0
nutawut2539 <34149513+nutawut2539@users.noreply.github.com>
2018-06-18 14:32:44 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,38 +1,41 @@
-<?php
-   if(isset($_FILES['image'])){
-      $errors= array();
-      $img = $_FILES['file_uploaded']['name'];
-      $file_name = $_FILES['image']['name'];
-      $file_size =$_FILES['image']['size'];
-      $file_tmp =$_FILES['image']['tmp_name'];
-      $file_type=$_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-      $expensions= array("jpeg","jpg","png");
-      if(in_array($file_ext,$expensions)=== false){
-         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-      }
-      if($file_size>2097152){
-         $errors[]="File size must be excately 2 MB";
-      }
-      if(empty($errors)==true){
-         echo "Success...<br>";
-	$a = $_FILES['image']['name'];
-	//trained tensorflow
-	$train = shell_exec("python tensorflow-master/tensorflow/examples/label_image/label_image.py \--graph=/tmp/output_graph.pb --labels=/tmp/output_labels.txt \--input_layer=Mul \--output_layer=final_result \--input_mean=128 --input_std=128 \--image=$a");
-    echo "the train-tensorflow is ".$train."<br>";
-      }else{print_r($errors);}
-   }
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
+
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
+
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
+}
+else
+{
+	echo "Method not allowed";
+}
+
 ?>
-<html>
-   <head>
-   <title>
-   Tensorflow-Project
-   </title>
-   </head>
-   <body>
-      <form action="" method="POST" enctype="multipart/form-data">
-         <input type="file" name="image" /><br>
-         <input type="submit"/>
-      </form>
-   </body>
-</html>
