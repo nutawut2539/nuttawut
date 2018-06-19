1a43dd7509ef91254e340a2b6944d0c147bcfc93
nutawut2539 <34149513+nutawut2539@users.noreply.github.com>
2018-06-19 17:49:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,11 @@
-<?php
-function processMessage($update) {
-    if($update["result"]["action"] == "sayHello"){
-        sendMessage(array(
-            "source" => $update["result"]["source"],
-            "speech" => "Hello from webhook",
-            "displayText" => "Hello from webhook",
-            "contextOut" => array()
-        ));
-    }
-}
-
-function sendMessage($parameters) {
-    echo json_encode($parameters);
-}
-
-$update_response = file_get_contents("php://input");
-$update = json_decode($update_response, true);
-if (isset($update["result"]["action"])) {
-    processMessage($update);
-}
+<?php 
+header('Content-Type: application/json');
+ob_start();
+$json = file_get_contents('php://input');
+$request = json_decode($json, true);
+$action = $request["result"]["action"];
+$parameters = $request["result"]["parameters"];
+[Code to set $outputtext, $nextcontext, $param1, $param2 values] $output["contextOut"] = array(array("name" => "$next-context", "parameters" => array("param1" => $param1value, "param2" => $param2value)));
+$output["speech"] = $outputtext; $output["displayText"] = $outputtext; $output["source"] = "whatever.php"; ob_end_clean();
+echo json_encode($output); 
 ?>
-
-
