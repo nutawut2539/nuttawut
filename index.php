172ccf575e0aa96de18cb84a4c29c9606579bcad
nutawut2539 <34149513+nutawut2539@users.noreply.github.com>
2018-06-19 15:55:21 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
-
+function getTeamInfo(req,res)
+{
+let teamToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.team ? req.body.result.parameters.team : 'Unknown';
+TeamInfo.findOne({name:teamToSearch},function(err,teamExists)
+      {
+        if (err)
+        {
+          return res.json({
+              speech: 'Something went wrong!',
+              displayText: 'Something went wrong!',
+              source: 'team info'
+          });
+        }
+if (teamExists)
+        {
+          return res.json({
+                speech: teamExists.description,
+                displayText: teamExists.description,
+                source: 'team info'
+            });
+        }
+        else {
+          return res.json({
+                speech: 'Currently I am not having information about this team',
+                displayText: 'Currently I am not having information about this team',
+                source: 'team info'
+            });
+        }
+      });
+}
