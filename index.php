00c8ac748c86ebe9f8191141a01fb0deca494e7d
nutawut2539 <34149513+nutawut2539@users.noreply.github.com>
2018-06-19 17:45:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,19 @@
 <?php
 function processMessage($update) {
-    if($update["result"]["action"] == "buscar.nfe"){
-    $response = new \stdClass();
-	$response->speech = "..........TEXT HERE...........";
-	$response->displayText = ".........TEXT HERE...........";
-	$response->source = $update["result"]["source"];
-	echo json_encode($response);
+    if($update["result"]["action"] == "sayHello"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "Hello from webhook",
+            "displayText" => "Hello from webhook",
+            "contextOut" => array()
+        ));
     }
 }
 
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
 
-/*
- * PEGANDO A REQUISIÇÃO
- */
 $update_response = file_get_contents("php://input");
 $update = json_decode($update_response, true);
 if (isset($update["result"]["action"])) {
