6a1f7b5fe9b648289a38f6d2cb6ffd8cae1981df
nutawut2539 <34149513+nutawut2539@users.noreply.github.com>
2018-06-19 16:51:04 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,27 @@
-$json = json_encode([
-                    'speech'   => $speech,
-                    'displayText' => $text,
-                    'data' => [],
-                    'contextOut' => [$context],
-                    'source' => $source
-            ]);
+<?php
+function processMessage($update) {
+    if($update["result"]["action"] == "buscar.nfe"){
+        sendMessage(array(
+            "source" => $update["result"]["source"],
+            "speech" => "..........TEXT HERE...........",
+            "displayText" => ".........TEXT HERE...........",
+            "contextOut" => array()
+        ));
+    }
+}
+/*
+ * FUNÇÃO PARA ENVIAR A MENSAGEM
+ */
+function sendMessage($parameters) {
+    echo json_encode($parameters);
+}
+
+/*
+ * PEGANDO A REQUISIÇÃO
+ */
+$update_response = file_get_contents("php://input");
+$update = json_decode($update_response, true);
+if (isset($update["result"]["action"])) {
+    processMessage($update);
+}
+?>
