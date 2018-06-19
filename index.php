6ef4fae54631f6c2d871a06b36d0feaf05c78e85
nutawut2539 <34149513+nutawut2539@users.noreply.github.com>
2018-06-19 17:11:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,10 @@
 function processMessage($update) {
     if($update["result"]["action"] == "buscar.nfe"){
         sendMessage(array(
-            'source' => $update["result"]["source"],
-            'speech' => "..........TEXT HERE...........",
-            'displayText' => ".........TEXT HERE...........",
-            'data' => [],
-            'contextOut' => array()
+            "source" => "..........TEXT HERE...........",
+            "speech" => "..........TEXT HERE...........",
+            "displayText" => ".........TEXT HERE...........",
+            "contextOut" => array()
         ));
     }
 }
