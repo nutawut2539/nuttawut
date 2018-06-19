4173977cdb1f8682733e6cb8ec38bf2d2107af6e
nutawut2539 <34149513+nutawut2539@users.noreply.github.com>
2018-06-19 16:14:39 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-function getTeamInfo(req,res)
-{
-let teamToSearch = req.body.result && req.body.result.parameters && req.body.result.parameters.team ? req.body.result.parameters.team : 'Unknown';
-TeamInfo.findOne({name:teamToSearch},function(err,teamExists)
-      {
-        if (err)
-        {
-          return res.json({
-              speech: 'Something went wrong!',
-              displayText: 'Something went wrong!',
-              source: 'team info'
-          });
-        }
-if (teamExists)
-        {
-          return res.json({
-                speech: teamExists.description,
-                displayText: teamExists.description,
-                source: 'team info'
-            });
-        }
-        else {
-          return res.json({
-                speech: 'Currently I am not having information about this team',
-                displayText: 'Currently I am not having information about this team',
-                source: 'team info'
-            });
-        }
-      });
+<?php 
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+// Process only when method is POST
+if($method == 'POST'){
+	$requestBody = file_get_contents('php://input');
+	$json = json_decode($requestBody);
+
+	$text = $json->result->parameters->text;
+
+	switch ($text) {
+		case 'hi':
+			$speech = "Hi, Nice to meet you";
+			break;
+
+		case 'bye':
+			$speech = "Bye, good night";
+			break;
+
+		case 'anything':
+			$speech = "Yes, you can type anything here.";
+			break;
+		
+		default:
+			$speech = "Sorry, I didnt get that. Please ask me something else.";
+			break;
+	}
+
+	$response = new \stdClass();
+	$response->speech = $speech;
+	$response->displayText = $speech;
+	$response->source = "webhook";
+	echo json_encode($response);
 }
+else
+{
+	echo "Method not allowed";
+}
+
+?>
