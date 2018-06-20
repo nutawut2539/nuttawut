925b31d21fcf7748164d14faf6ff1c506e6a6b01
nutawut2539 <34149513+nutawut2539@users.noreply.github.com>
2018-06-20 10:18:46 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,23 @@
 <?php 
-
 $method = $_SERVER['REQUEST_METHOD'];
-
-// Process only when method is POST
 if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	$json = json_decode($requestBody);
-
 	$text = $json->result->parameters->text;
-
 	switch ($text) {
 		case 'hi':
 			$speech = "Hi, Nice to meet you";
 			break;
-
 		case 'bye':
 			$speech = "Bye, good night";
 			break;
-
 		case 'anything':
 			$speech = "Yes, you can type anything here.";
-			break;
-		
+			break;		
 		default:
 			$speech = "Sorry, I didnt get that. Please ask me something else.";
 			break;
 	}
-
 	$response = new \stdClass();
 	$response->speech = $speech;
 	$response->displayText = $speech;
@@ -37,5 +28,4 @@ else
 {
 	echo "Method not allowed";
 }
-
 ?>
